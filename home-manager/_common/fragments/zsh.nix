f0c6167ccd7054a921997a1b83f8f4cdf0bf83ee
udf <tabhooked@gmail.com>
2026-02-08 21:51:19 +0200

    make zsh home-manager optional

--- home-manager/_common/fragments/zsh.nix
+++ home-manager/_common/fragments/zsh.nix
@@ -4,35 +4,44 @@
   pkgs,
   ...
 }:
-
+with lib;
 let
   zshrc = (import ../../../nixos/_common/fragments/zsh/zshrc.nix) { inherit lib pkgs; };
+  cfg = config.programs.zsh-config;
 in
 {
-  programs.zsh = {
-    enable = true;
-    history = {
-      append = false;
-      ignoreDups = false;
-      ignoreAllDups = false;
-      saveNoDups = false;
-      findNoDups = false;
-      ignoreSpace = false;
-      expireDuplicatesFirst = false;
-      share = false;
-      extended = false;
-    };
-    completionInit = "";
-    envExtra = "skip_global_compinit=1";
-    initContent = lib.mkMerge [
-      (lib.mkOrder 1000 zshrc)
-      (lib.mkOrder 1001 ''
-        # Add scripts directory to path
-        export PATH="$HOME/scripts:$PATH"
+  options.programs.zsh-config.enable = mkOption {
+    default = true;
+    description = "Enable adding zsh config based on nixos system configs";
+    type = lib.types.bool;
+  };
 
-        # Add npm package binaries to path
-        export PATH="$HOME/.npm-packages/bin:$PATH"
-      '')
-    ];
+  config = mkIf cfg.enable {
+    programs.zsh = {
+      enable = true;
+      history = {
+        append = false;
+        ignoreDups = false;
+        ignoreAllDups = false;
+        saveNoDups = false;
+        findNoDups = false;
+        ignoreSpace = false;
+        expireDuplicatesFirst = false;
+        share = false;
+        extended = false;
+      };
+      completionInit = "";
+      envExtra = "skip_global_compinit=1";
+      initContent = lib.mkMerge [
+        (lib.mkOrder 1000 zshrc)
+        (lib.mkOrder 1001 ''
+          # Add scripts directory to path
+          export PATH="$HOME/scripts:$PATH"
+
+          # Add npm package binaries to path
+          export PATH="$HOME/.npm-packages/bin:$PATH"
+        '')
+      ];
+    };
   };
 }
