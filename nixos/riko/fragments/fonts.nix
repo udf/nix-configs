192f4e77980d1bee9e39e78fb70ff4f57658e2bf
udf <tabhooked@gmail.com>
2026-02-15 00:25:05 +0200

    add font config

---
+++ nixos/riko/fragments/fonts.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+{
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
+      font-awesome
+      source-han-sans
+      source-han-serif
+      hack-font
+    ];
+    fontconfig.defaultFonts = {
+      serif = [
+        "Noto Serif"
+        "Noto Serif CJK"
+        "Source Han Serif"
+      ];
+      sansSerif = [
+        "Noto Sans"
+        "Noto Sans CJK"
+        "Source Han Sans"
+      ];
+      monospace = [
+        "Hack"
+      ];
+    };
+  };
+}
