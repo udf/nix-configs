1e0e43cad0d4995cf218df7f49d4505ea82aa9e6
udf <tabhooked@gmail.com>
2026-02-15 22:32:21 +0200

    source home manager vars

--- nixos/riko/fragments/home-manager.nix
+++ nixos/riko/fragments/home-manager.nix
@@ -2,4 +2,7 @@
 {
   imports = [ <home-manager/nixos> ];
   home-manager.users.sam = import ../../../home-manager/riko/home.nix;
+  programs.zsh.interactiveShellInit = ''
+    . ~"/.nix-profile/etc/profile.d/hm-session-vars.sh"
+  '';
 }
