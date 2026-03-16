9656ae0331c696522df073cc26e55b79763b2149
udf <tabhooked@gmail.com>
2026-03-16 21:34:37 +0200

    add untracked files when syncing

--- sync.sh
+++ sync.sh
@@ -2,6 +2,8 @@
 
 [ -z "$1" ] && echo "No host supplied" && exit 1
 
+git add -N .
+
 for host in "$@"; do
   echo "Syncing to $host..."
   rsync -rah --info=progress2 --exclude=flake.lock --delete ./ $host:~/nixos/
