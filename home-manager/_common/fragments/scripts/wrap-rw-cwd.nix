e96f350f28157f83c3310cccbace4d7bf12737ae
udf <tabhooked@gmail.com>
2026-04-17 19:49:26 +0200

    use system bwrap if possible

--- home-manager/_common/fragments/scripts/wrap-rw-cwd.nix
+++ home-manager/_common/fragments/scripts/wrap-rw-cwd.nix
@@ -1,7 +1,12 @@
 { lib, pkgs, ... }:
 {
   namedPackages.ro-cwd = pkgs.writeShellScriptBin "wrap-rw-cwd" ''
-    ${lib.getExe pkgs.bubblewrap} \
+    bwrap_bin="$(command -v bwrap 2>/dev/null || true)"
+    if [ -z "$bwrap_bin" ]; then
+      bwrap_bin="${lib.getExe pkgs.bubblewrap}"
+    fi
+
+    exec "$bwrap_bin" \
       --ro-bind / / \
       --bind "$PWD" "$PWD" \
       --dev /dev \
