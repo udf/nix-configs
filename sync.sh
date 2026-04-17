7ad4603b9c7c16bef5c04dc453b4245737878698
udf <tabhooked@gmail.com>
2026-04-17 20:35:43 +0200

    only skip root flake

--- sync.sh
+++ sync.sh
@@ -6,5 +6,5 @@ git add -N .
 
 for host in "$@"; do
   echo "Syncing to $host..."
-  rsync -rah --info=progress2 --exclude=flake.lock --delete ./ $host:~/nixos/
+  rsync -rah --info=progress2 --exclude=/flake.lock --delete ./ $host:~/nixos/
 done
