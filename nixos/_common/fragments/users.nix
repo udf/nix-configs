6f12d2023dbff76f6444d08ab5ed590775e23d6f
udf <tabhooked@gmail.com>
2026-04-17 19:50:09 +0200

    goodbye tim

--- nixos/_common/fragments/users.nix
+++ nixos/_common/fragments/users.nix
@@ -6,7 +6,7 @@
       extraGroups = [ "wheel" ];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIzlWx6yy2nWV8fYcIm9Laap8/KxAlLJd943TIrcldSY archdesktop"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKxBxAt/K3h+W4wyxLbTsW0awTIzJy2rpsQgDKxBHNe5 iOS"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB/M7Ba3GQSuRFjMTInCAv/mZIvlc4KxyrJZkklL0yhv android"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEbHvYANduDUO7939VJu12KFxMEM5fCRx/4PG/W5UwIa sam@mashiro"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKXvppyCaen/sIcyxeR/ZRhRhe3/J+8t6f0XFXh/E5b9 sam@riko"
       ];
