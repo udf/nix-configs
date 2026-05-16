b50aae44723498d6109dd2f61b04ba447847c05d
udf <tabhooked@gmail.com>
2026-05-16 23:32:05 +0200

    use updated mpdris

--- home-manager/_common/fragments/mpdris2/default.nix
+++ home-manager/_common/fragments/mpdris2/default.nix
@@ -2,5 +2,12 @@
 {
   services.mpdris2.package = pkgs.mpdris2.overrideAttrs (old: {
     patches = (old.patches or [ ]) ++ [ ./more-sane-interval.patch ];
+    version = "0.9.1-rc";
+    src = pkgs.fetchFromGitHub {
+      owner = "eonpatapon";
+      repo = "mpDris2";
+      rev = "d73f32c2b74528e94032331cd6cecb0b7e7bcada";
+      hash = "sha256-J0OApalIqYCGiaxuE/10K6n2GyP6WoZHF7NcRG3FXnA=";
+    };
   });
 }
