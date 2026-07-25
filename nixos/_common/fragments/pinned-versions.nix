a4889f1852a5a41ee033d51576aeeef56f56616a
udf <tabhooked@gmail.com>
2026-07-25 23:10:13 +0200

    upgrade containers

--- nixos/_common/fragments/pinned-versions.nix
+++ nixos/_common/fragments/pinned-versions.nix
@@ -9,9 +9,9 @@
       pihole = "pihole/pihole:2026.04.1";
       nebula-sync = "ghcr.io/lovelaze/nebula-sync:v0.11.2";
       nextcloud-aio-imaginary = "nextcloud/aio-imaginary:20260527_140826";
-      suwayomi-tachidesk = "ghcr.io/suwayomi/tachidesk:v2.2.2194";
+      suwayomi-tachidesk = "ghcr.io/suwayomi/tachidesk:v2.3.2243";
       szuru-postgres = "postgres:16-alpine";
-      frigate = "ghcr.io/blakeblackshear/frigate:0.17.1";
+      frigate = "ghcr.io/blakeblackshear/frigate:0.17.2";
     };
     programs = {
       nicotine-plus = "3.3.10";
