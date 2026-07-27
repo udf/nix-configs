0b0048ea8a3bb285511cbfd20a3fac20b7ed21d8
udf <tabhooked@gmail.com>
2026-07-27 22:42:59 +0200

    use catppuccin for sway

---
+++ home-manager/riko/fragments/catppuccin-sway.nix
@@ -0,0 +1,50 @@
+{ inputs, lib, ... }:
+let
+  catppucinOptions = import (inputs.self + "/common/catppuccin-options.nix") { inherit lib; };
+in
+{
+  wayland.windowManager.sway.config.colors =
+    let
+      accentColour = "$" + catppucinOptions.accent;
+      urgentColor = "$red";
+      indicatorColour = "$peach";
+    in
+    {
+      focused = {
+        border = accentColour;
+        background = accentColour;
+        text = "$base";
+        indicator = indicatorColour;
+        childBorder = accentColour;
+      };
+      focusedInactive = {
+        border = accentColour;
+        background = "$base";
+        text = "$text";
+        indicator = indicatorColour;
+        childBorder = "$overlay0";
+      };
+      unfocused = {
+        border = "$overlay0";
+        background = "$base";
+        text = "$text";
+        indicator = indicatorColour;
+        childBorder = "$overlay0";
+      };
+      urgent = {
+        border = urgentColor;
+        background = urgentColor;
+        text = "$base";
+        indicator = "$overlay0";
+        childBorder = urgentColor;
+      };
+      placeholder = {
+        border = "$overlay0";
+        background = "$base";
+        text = "$text";
+        indicator = "$overlay0";
+        childBorder = "$overlay0";
+      };
+      background = "$base";
+    };
+}
