258fd028c87f67a85f0e524e2f8b60b93da94aae
udf <tabhooked@gmail.com>
2026-09-07 01:56:02 +0200

    update

--- nixos/_common/fragments/pinned-versions.nix
+++ nixos/_common/fragments/pinned-versions.nix
@@ -6,10 +6,10 @@
   custom.pinnedVersions = {
     containers = {
       diun = "crazymax/diun:4.31";
-      pihole = "pihole/pihole:2026.04.1";
+      pihole = "pihole/pihole:2026.07.2";
       nebula-sync = "ghcr.io/lovelaze/nebula-sync:v0.11.2";
       nextcloud-aio-imaginary = "nextcloud/aio-imaginary:20260527_140826";
-      suwayomi-tachidesk = "ghcr.io/suwayomi/tachidesk:v2.3.2243";
+      suwayomi-tachidesk = "ghcr.io/suwayomi/tachidesk:v2.3.2359";
       szuru-postgres = "postgres:16-alpine";
       frigate = "ghcr.io/blakeblackshear/frigate:0.17.2";
     };
