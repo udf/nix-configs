eab3539544df7801c245ce919344719ff7286f62
udf <tabhooked@gmail.com>
2026-09-07 04:06:50 +0200

    (partially) update ananke

--- nixos/ananke/flake.nix
+++ nixos/ananke/flake.nix
@@ -4,7 +4,8 @@
   inputs = {
     # MARK: pinned version
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    # TODO: unpin this back to /master
+    nixos-hardware.url = "github:NixOS/nixos-hardware/7aefd9ab01eef691c5c688d9ca4d1ea003cca284";
   };
 
   outputs = inputs: {
